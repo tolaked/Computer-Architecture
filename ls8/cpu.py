7f0901b77f8394d476689a5bf6d7dce737317f77
Akere Adetola <adetolaakere1@gmail.com>
2020-03-24 13:05:52 +0100

    refactored task day 1 code

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -26,9 +26,6 @@ class CPU:
         """Load a program into memory."""
 
         address = 0
-
-        # For now, we've just hardcoded a program:
-
         program = [
             # From print8.ls8
             0b10000010, # LDI R0,8
@@ -42,7 +39,6 @@ class CPU:
         for instruction in program:
             self.ram[address] = instruction
             address += 1
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -75,4 +71,19 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+        while running:
+            ir = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            if ir == HLT:
+                running = False
+            elif ir == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif ir == PRN:
+                print(self.reg[operand_a])
+                self.pc +=2
